79868042b169dbaf0aeb2112152ebef9be6063da
Andrey Larin <lestex@gmail.com>
2018-06-25 12:20:45 +0300

    adds config npm module and basic configuration for different environments

--- config/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    secret: process.env.NODE_ENV === 'production' ? process.env.SECRET : 'dev_secret',
-    dbUrl: process.env.DB_URL ? process.env.DB_URL : 'mongodb://localhost/realworld_test',
-    port: process.env.PORT ? process.env.PORT : 3000,
-    isProduction: process.env.NODE_ENV === 'production' ? true : false
-};
