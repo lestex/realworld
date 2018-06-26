d91f606c8bc14009460246ec25e0e4a15ff2da9e
Andrey Larin <lestex@gmail.com>
2018-06-26 22:55:33 +0300

    adds route for login

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,21 +5,27 @@ router.get('/user', (req, res) => {
     res.send('You got to user');
 });
 
-// POST to /api/users
-router.post('/users', async (req, res, next) => {
+// POST to /api/users for sign up
+router.post('/users', (req, res, next) => {
     const user = new User();
   
     user.username = req.body.user.username;
     user.email = req.body.user.email;
     user.password = req.body.user.password;
 
-    try {
-        const result = await user.save();
+    user.save().then(() => { 
+        return res.json({user: user.toJSON()});
+    }).catch(next);   
+});
 
-        return res.json({user: result.toJSON()});        
-    } catch (error) {
-        return next();
-    }
+// POST /api/users/login for log in
+router.post('/users/login', function(req, res, next){
+    // validate user object
+    
+    // authenticate user
+
+    // return JWT + user 
+    // or validation error
 });
 
 module.exports = router;
