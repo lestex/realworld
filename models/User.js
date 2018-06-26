f60226f37dda1186decca3198bdbd2b918f739b0
Andrey Larin <lestex@gmail.com>
2018-06-26 22:18:44 +0300

    adds User model and POST to users handler

--- models/User.js
+++ models/User.js
@@ -1,10 +1,18 @@
 const mongoose = require('mongoose');
-const {Schema} = mongoose.Schema;
 
-const UserSchema = new Schema({
+let UserSchema = new mongoose.Schema({
     password: String,
     name: String,    
     email: String
-});
+}, {timestamps: true});
 
-module.exports = UserSchema;
+UserSchema.methods.toJSON = function() {
+    return {
+        username: this.username,
+        email: this.email
+    };
+};
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,25 @@
 const router = require('express').Router();
+const User = require('../../models/User');
 
 router.get('/user', (req, res) => {
     res.send('You got to user');
+});
+
+// POST to /api/users
+router.post('/users', async (req, res, next) => {
+    let user = new User();
+  
+    user.username = req.body.user.username;
+    user.email = req.body.user.email;
+    user.password = req.body.user.password;
+
+    try {
+        const result = await user.save();
+        return res.json({user: result.toJSON()});        
+    }
+    catch {
+        next();
+    }    
 });
 
 module.exports = router;
