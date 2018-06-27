ff31a32d31477f477b280fbd0646907998f4e3c6
Andrey Larin <lestex@gmail.com>
2018-06-27 17:43:10 +0300

    adds required and generateJWT to User

--- models/User.js
+++ models/User.js
@@ -1,18 +1,43 @@
 const mongoose = require('mongoose');
 const Joi = require('joi');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 const UserSchema = new mongoose.Schema({
-    password: String,
-    username: String,    
+    username: {
+        type: String,
+        lowercase: true,
+        required: [true, "can't be blank"]
+    },
+    password: {
+        type: String,        
+        required: [true, "can't be blank"]
+    },
     image: String,
-    email: String,
+    email: {
+        type: String,
+        lowercase: true,
+        required: [true, "can't be blank"]
+    },
     bio: String
 }, {timestamps: true});
+
+UserSchema.methods.generateJWT = function() {
+    var today = new Date();
+    var exp = new Date(today);
+    exp.setDate(today.getDate() + 60);
+    return jwt.sign({
+        id: this._id,
+        username: this.username,
+        exp: parseInt(exp.getTime() / 1000),
+    }, config.secret);
+};
 
 UserSchema.methods.toJSON = function() {
     return {
         username: this.username,
-        email: this.email
+        email: this.email,
+        token: this.generateJWT()
     };
 };
 
@@ -40,6 +65,6 @@ function validateUser(user) {
   
     return Joi.validate(user, schema);
 }
-  
+
 exports.User = User; 
 exports.validate = validateUser;
