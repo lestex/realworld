b049f90fb4fb02499a010286e3be76c6c50373d6
Andrey Larin <lestex@gmail.com>
2018-06-27 19:48:29 +0300

    updates API and tests

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,13 @@
 const router = require('express').Router();
 const {User} = require('../../models/User');
 
-router.post('/users', (req, res, next) => {
-    const user = new User();
-    user.username = req.body.user.username;
-    user.email = req.body.user.email;
-    user.password = req.body.user.password;
+// POST to /api/users/ for sign up
+router.post('/users', async (req, res, next) => {
+    const user = new User({
+        username: req.body.user.username,
+        email: req.body.user.email,
+        password: req.body.user.password
+    });
 
     user.save().then(function() { 
         return res.json({user: user.toJSON()});
@@ -15,13 +17,20 @@ router.post('/users', (req, res, next) => {
 
 // POST /api/users/login for log in
 router.post('/users/login', function(req, res) {
-    // validate user object
+    if(!req.body.user.email){
+        return res.status(422).json({errors: {email: "can't be blank"}});
+    }
+    
+    if(!req.body.user.password){
+        return res.status(422).json({errors: {password: "can't be blank"}});
+    }
     
     // authenticate user
 
     // return JWT + user 
     // or validation error
-    res.send('You got to users/login');
+    return res.status(200).send('OK')
+    
 });
 
 module.exports = router;
--- tests/integration/users.test.js
+++ tests/integration/users.test.js
@@ -1,9 +1,12 @@
 const request = require('supertest');
+const {User} = require('../../models/User')
+
 let server;
 
 describe('/api/users', () => {
     beforeEach(() => { server = require('../../app'); })
     afterEach(() => { server.close(); })
+    afterAll(() => { User.collection.drop(); })
 
     describe('POST /', () => {
         it('should return 422 if username not provided', async () => {
@@ -17,9 +20,7 @@ describe('/api/users', () => {
             expect(res.body).toHaveProperty('errors');
             expect(res.body.errors).toHaveProperty('username');
         });
-    })
 
-    describe('POST /', () => {
         it('should return 422 if password not provided', async () => {
             const invalidUser = { user: {
                 username: "testUser",
@@ -31,9 +32,7 @@ describe('/api/users', () => {
             expect(res.body).toHaveProperty('errors');
             expect(res.body.errors).toHaveProperty('password');
         });
-    })
 
-    describe('POST /', () => {
         it('should return 422 if email not provided', async () => {
             const invalidUser = { user: {
                 username: "testUser",
@@ -45,9 +44,7 @@ describe('/api/users', () => {
             expect(res.body).toHaveProperty('errors');
             expect(res.body.errors).toHaveProperty('email');
         });
-    })
 
-    describe('POST /', () => {
         it('should return user with username email and token', async () => {
             const User = { user: {
                                 username: "testUser",
@@ -59,6 +56,25 @@ describe('/api/users', () => {
             expect(res.body.user).toHaveProperty('username');
             expect(res.body.user).toHaveProperty('email');
             expect(res.body.user).toHaveProperty('token');
+        });
+    })
+
+    // login 
+    describe('POST /login', () => {
+        it('should return 200 if username and password are correct', async () => {
+            const user = new User({
+                username: "testUser",
+                email: "user@email.com",
+                password: "123456"
+            });
+            user.save();
+            const apiUser = { user: {
+                username: "testUser",
+                email: "user@email.com",
+                password: "123456"
+            }};
+            const res = await request(server).post('/api/users/login').send(apiUser);
+            expect(res.status).toBe(200);
         });
     })
 });
